f365f9dc647f4bc99d2f23deb19de071aadb06cc
carol <freitascaroline49@gmail.com>
2025-09-21 10:35:51 -0300

    ✨feat: implementação das telas estáticas do módulo de cadastro

---
+++ sistemarh/src/main/java/com/example/sistemarh/InterfaceController.java
@@ -0,0 +1,98 @@
+package com.example.sistemarh;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class InterfaceController {
+
+    @GetMapping("/")
+    public String paginaInicial(Model model) {
+        return "index";
+    }
+
+    @GetMapping("/paginaAdm")
+    public String mostrarPagina2() {
+        return "paginaAdm";
+    }
+
+    // == ROTAS DE CADASTRO (ALUNO 2) ==
+    @GetMapping("/cadastro")
+    public String menuCadastro() {return "cadastro/menu";
+    }
+
+    @GetMapping("/cadastro/candidato")
+    public String cadastroCandidato() {return "cadastro/cadastroCandidato";
+    }
+
+    @GetMapping("/cadastro/candidatura")
+    public String candidaturaVaga() {return "cadastro/candidaturaVaga";
+    }
+
+    @GetMapping("/cadastro/status")
+    public String statusCandidatura() {return "cadastro/statusCandidatura";
+    }
+
+     // == ROTAS DE RECRUTAMENTO (ALUNO 3) ==
+    @GetMapping("/recrutamento")
+    public String menuRecrutamento() {
+        return "recrutamento/menu";
+    }
+
+    @GetMapping("/recrutamento/cadastrar-candidato")
+    public String cadastrarCandidato() {
+        return "recrutamento/cadastro-candidato";
+    }
+
+    @GetMapping("/recrutamento/realizar-candidatura")
+    public String realizarCandidatura() {
+        return "recrutamento/realizar-candidatura";
+    }
+
+    @GetMapping("/recrutamento/marcar-entrevista")
+    public String marcarEntrevista() {
+        return "recrutamento/marcar-entrevista";
+    }
+
+    @GetMapping("/recrutamento/solicitar-contratacao")
+    public String solicitarContratacao() {
+        return "recrutamento/solicitar-contratacao";
+    }
+
+    @GetMapping("/recrutamento/consultar-contratacoes")
+    public String consultarContratacoes() {
+        return "recrutamento/consultar-contratacoes";
+    }
+
+    // == ROTAS FINANCEIRO (ALUNO 4) ==
+    @GetMapping("/financeiro")
+    public String menuFinanceiro() {
+        return "financeiro/menu";
+    }
+
+    @GetMapping("/financeiro/cadastrar-funcionario")
+    public String cadastrarFuncionario() {
+        return "financeiro/cadastro-funcionario";
+    }
+
+    @GetMapping("/financeiro/configurar-regras")
+    public String configurarRegras() {
+        return "financeiro/configurar-regras";
+    }
+
+    @GetMapping("/financeiro/gerar-folha")
+    public String gerarFolha() {
+        return "financeiro/gerar-folha";
+    }
+
+    @GetMapping("/financeiro/relatorio")
+    public String relatorioFinanceiro() {
+        return "financeiro/relatorio";
+    }
+
+    @GetMapping("/financeiro/contracheques")
+    public String contracheques() {
+        return "financeiro/contracheques";
+    }
+}
---
+++ sistemarh/src/main/java/com/example/sistemarh/SistemarhApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistemarh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemarhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemarhApplication.class, args);
+	}
+
+}
